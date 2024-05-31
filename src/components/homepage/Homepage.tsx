bae0eae441761bd96d1a3e46317f1e3ca20f6cd4
Ales Zima <ales@cpex.cz>
2024-05-31 14:32:30 +0200

    Added Header to MainLayout

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,6 +1,5 @@
 import styles from './Homepage.module.css';
 
-import Header from '../header/Header';
 import SearchBar from './SearchBar';
 import RegionFilter from './RegionFilter';
 import type { Country } from '../../types/country';
@@ -13,19 +12,16 @@ type HomepageProps = {
 export default function Homepage({ countries }: HomepageProps) {
   countries.sort((a, b) => b.population - a.population);
   return (
-    <>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.flex}>
-          <SearchBar />
-          <RegionFilter />
-        </div>
-        <div className={styles.grid}>
-          {countries.map((country) => (
-            <CountryCard key={country.alpha3Code} country={country} />
-          ))}
-        </div>
-      </main>
-    </>
+    <main className={styles.main}>
+      <div className={styles.flex}>
+        <SearchBar />
+        <RegionFilter />
+      </div>
+      <div className={styles.grid}>
+        {countries.map((country) => (
+          <CountryCard key={country.alpha3Code} country={country} />
+        ))}
+      </div>
+    </main>
   );
 }
