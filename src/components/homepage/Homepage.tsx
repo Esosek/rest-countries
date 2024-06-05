ff00eff2af3495048b937d077faa47fbd29e3a02
Ales Zima <ales@cpex.cz>
2024-06-05 18:38:28 +0200

    Added Region filters

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -4,15 +4,22 @@ import SearchBar from './SearchBar';
 import RegionFilter from './RegionFilter';
 import type { Country } from '../../types/country';
 import CountryCard from './CountryCard';
+import { useState } from 'react';
 
 type HomepageProps = {
   countries: Country[];
 };
 
 export default function Homepage({ countries }: HomepageProps) {
+  const [filteredCountries, setFilteredCountries] = useState(countries);
+
   countries.sort((a, b) => b.population - a.population);
 
-  function handleFilterChange(value: string) {}
+  function handleFilterChange(filteredRegion: string) {
+    setFilteredCountries(
+      countries.filter((country) => country.region === filteredRegion)
+    );
+  }
   return (
     <main className={styles.main}>
       <div className={styles.flex}>
@@ -20,7 +27,7 @@ export default function Homepage({ countries }: HomepageProps) {
         <RegionFilter countries={countries} onChange={handleFilterChange} />
       </div>
       <div className={styles.grid}>
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <CountryCard key={country.alpha3Code} country={country} />
         ))}
       </div>
